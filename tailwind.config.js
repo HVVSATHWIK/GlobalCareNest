6a6eede260498ca0960f989a5680b884186970e6
Veer <146707877+HVVSATHWIK@users.noreply.github.com>
2024-11-12 21:55:50 +0530

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#219B9D',
+        secondary: '#B9E5E8',
+      },
+    },
   },
   plugins: [],
 };
