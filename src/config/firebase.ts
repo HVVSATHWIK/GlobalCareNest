78ee920f171bea96cdedce475a37356b67c77576
Veer <146707877+HVVSATHWIK@users.noreply.github.com>
2024-12-16 01:57:50 +0530

    Add files via upload

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,13 +1,22 @@
-import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
-import { getStorage } from 'firebase/storage';
-import { getAnalytics } from 'firebase/analytics';
-import firebaseConfig from './firebase.config';
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyC-ualGLZVv2O-B34JG4S7_tZBsN_8Q8W0",
+  authDomain: "global-carenest.firebaseapp.com",
+  projectId: "global-carenest",
+  storageBucket: "global-carenest.firebasestorage.app",
+  messagingSenderId: "843009572087",
+  appId: "1:843009572087:web:51b6570a5b8253bde5e7f2",
+  measurementId: "G-SED50ZVK4P"
+};
+
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
+export const analytics = getAnalytics(app);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
-export const analytics = getAnalytics(app);
